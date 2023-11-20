b5d4e8de321f4e16df26adb843991b15262711a5
Ashish <sokebat01@gmail.com>
2023-11-20 22:00:47 +0545

    learn async await

--- Callback Async,Promises/asyncAwait.js
+++ Callback Async,Promises/asyncAwait.js
@@ -20,7 +20,7 @@ async function getData() {
 
 }
 const dataPromise = getData();
-dataPromise.then((result) => console.log(result))
+dataPromise.then((result) => console.log(result)) 
 
 */
 
@@ -41,6 +41,7 @@ const dataPromise = getData()
 dataPromise.then((res) => console.log(res))
 
 */
+
 /*
 // handelling  promise with async await
 const p = new Promise((resolve, reject) => {
@@ -53,3 +54,78 @@ async function handelPromise() {
 handelPromise()
 
 */
+
+/*
+const p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("promise is resolved")
+    }, 10000)
+})
+
+
+// consuming promise using async await
+async function handelPromise() {
+    const value = await p;
+    console.log(" it wil be printed after resolving promises!!")
+    console.log(value);
+}
+handelPromise()
+
+
+// consuming promise using older way
+// function getData() {
+//     p.then((res) => console.log(res))
+//     console.log('js engine not wait to consume promise  and it prints this line!!')
+// }
+//getData()
+
+*/
+
+/*
+
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("promise is resolved1")
+    }, 5000)
+})
+
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => { resolve("promise is resolved2") }, 10000)
+})
+
+
+async function handelPromise() {
+    const val1 = await p1;
+    console.log(" it wil be printed after resolving promises 1!!")
+    console.log(val1);
+
+    const val2 = await p2;
+    console.log(" it wil be printed after resolving promises 2!!")
+    console.log(val2);
+
+}
+handelPromise()
+
+*/
+
+
+const API_URL = "https://api.github.com/users/sokebat"
+
+async function handelPromise() {
+    try {
+        const data = await fetch(API_URL)
+        const jsonValue = await data.json()
+        console.log(jsonValue)
+
+    } catch (error) {
+        console.log(error)
+
+    }
+
+    // fetch(API_URL)
+    //     .then((res) => res.json())
+    //     .then((jsonValue) => console.log(jsonValue));
+
+}
+
+handelPromise()
